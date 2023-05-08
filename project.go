e30377b466cf7ec9b0d87540b02429eaf36f2c14
Farooq <farooquememon385@gmail.com>
2023-05-08 11:27:27 +0500

    added loadTodos function to load todo tasks from file todo.txt

--- project.go
+++ project.go
@@ -6,6 +6,9 @@ import (
 	"os"
 )
 
+//the to do list is implemented with file handling
+const todoFile = "todo.txt"
+
 func main(){
 	fmt.Println("#######################################")
 	fmt.Println("******** Welcome to To-Do List ********")
@@ -18,4 +21,31 @@ func main(){
 	fmt.Print("Enter a command (add/remove/show/quit): ")
 	scanner.Scan()
 	command := scanner.Text()
+
+
+}
+
+// Load existing to-do items from file
+func loadTodos() []string {
+	var todos []string
+
+	file, err := os.Open(todoFileName)
+	if err != nil {
+		fmt.Println("No existing to-do list found. Starting with an empty list.")
+		return todos
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		todo := scanner.Text()
+		todos = append(todos, todo)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading to-do list:", err)
+	}
+
+	fmt.Println("To-do list loaded from file.")
+	return todos
 }
